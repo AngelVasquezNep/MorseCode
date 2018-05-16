27492212a978d64cd9f3688b6ece01dcbf149547
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2018-05-15 21:42:13 -0500

    Se modifica el acceso a los métodos

--- MorseCode.java
+++ MorseCode.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
 
 public class MorseCode {
+  public static String[] cMorse = { "   ", ".", "-", ".-", "---", "..", "-.", "...", "....", ".-.", "-..", ".-..", "..-", "-.-.",
+  "--", "..-.", ".--", "--.", ".--.", "-.--", "-...", "...-", "-.-", ".---", "--.-", "-..-", "--..", ".----",
+  "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", "/" };
+  
+  // String letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890";
+  public static String englishLetters = " ETAOINSHRDLUCMFWGPYBVKJQXZ1234567890 ";
+  
+  public static int position = 0;
+  public static String result = "";
+  public static String option;
+  public static String palabra = "";
+  public static Scanner entrada = new Scanner(System.in);
+
   public static void main(String[] args) {
-    Scanner entrada = new Scanner(System.in);
-    
-    String[] cMorse = { "   ", ".", "-", ".-", "---", "..", "-.", "...", "....", ".-.", "-..", ".-..", "..-", "-.-.",
-    "--", "..-.", ".--", "--.", ".--.", "-.--", "-...", "...-", "-.-", ".---", "--.-", "-..-", "--..", ".----",
-    "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", "/" };
-    
-    // String letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890";
-    String englishLetters = " ETAOINSHRDLUCMFWGPYBVKJQXZ1234567890 ";
-    
-    int position = 0;
-    String result = "";
-    String option;
-    String palabra = "";
-    
+    MorseCode.showMenu();
+  }
+
+  public static void showMenu () {
     System.out.println("Hola, Bienvenidos!");
     
     do {
@@ -29,48 +32,14 @@ public class MorseCode {
       
       switch (option) {
         case "1":
-        System.out.println("La ñ no forma parte del código así que no podrá escribir mensajes que la incluyan.");
-        System.out.print("Introduzca su mensaje: ");
-        
-        palabra = entrada.nextLine();
-        
-        for (int i = 0; i < palabra.length(); i++) {
-          position = englishLetters.indexOf(palabra.toUpperCase().charAt(i));
-          if( position == -1) {
-            result += "letra-error" + " ";
-          }else{
-            // position = englishLetters.indexOf(palabra.toUpperCase().charAt(i));
-            result += cMorse[position] + " ";
-          }
-        }
-        System.out.println("Su mensaje en morse: " + result);
-        
+
+          textToMorse();
+
         break;
-        
+
         case "2":
-        System.out.println("Recuerde separar cada letra con un espacio y cada palabra con el siguiente carácter: / ");
-        System.out.print("Introduzca su mensaje: ");
-        
-        palabra = entrada.nextLine();
-        
-        String[] arrayItem = {};
-        arrayItem = palabra.split(" ");
-        
-        System.out.print("Su mensaje en texto normal: ");
-        
-        byte cMorseSize = (byte) Morse.length;
-        
-        for (String o : arrayItem) {
-          for (int i = 0; i < cMorseSize; i++) {
-              if (o.equals(cMorse[i])) {
-                System.out.print(englishLetters.charAt(i));
-                break;
-              }
-              if(i == (cMorseSize - 1)) {
-                System.out.print( " code-error " );
-              }
-            }
-          }
+
+          morseToText();
 
         break;
 
@@ -91,4 +60,49 @@ public class MorseCode {
 
     } while ( !option.equals("3") );
   }
+
+  public static void textToMorse () {
+    System.out.println("La ñ no forma parte del código así que no podrá escribir mensajes que la incluyan.");
+    System.out.print("Introduzca su mensaje: ");
+    
+    palabra = entrada.nextLine();
+    
+    for (int i = 0; i < palabra.length(); i++) {
+      position = englishLetters.indexOf(palabra.toUpperCase().charAt(i));
+      if( position == -1) {
+        result += "letra-error" + " ";
+      }else{
+        // position = englishLetters.indexOf(palabra.toUpperCase().charAt(i));
+        result += cMorse[position] + " ";
+      }
+    }
+    System.out.println("Su mensaje en morse: " + result);
+  }
+
+  public static void morseToText () {
+    System.out.println("Recuerde separar cada letra con un espacio y cada palabra con el siguiente carácter: / ");
+    System.out.print("Introduzca su mensaje: ");
+    
+    palabra = entrada.nextLine();
+    
+    String[] arrayItem = {};
+    arrayItem = palabra.split(" ");
+    
+    System.out.print("Su mensaje en texto normal: ");
+    
+    byte cMorseSize = (byte) cMorse.length;
+    
+    for (String o : arrayItem) {
+      for (int i = 0; i < cMorseSize; i++) {
+        if (o.equals(cMorse[i])) {
+          System.out.print(englishLetters.charAt(i));
+          break;
+        }
+        if(i == (cMorseSize - 1)) {
+          System.out.print( " code-error " );
+        }
+      }
+    }
+  }
+
 }
